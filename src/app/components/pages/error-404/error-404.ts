700ed30c3708fea3df0cf9fe465d79e357abcd48
Andrew Shin <andyrpg@gmail.com>
2025-10-06 19:25:34 -0700

    designs, button, error page added

---
+++ src/app/components/pages/error-404/error-404.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { Button } from '../../shared/button/button';
+
+@Component({
+  selector: 'app-error-404',
+  imports: [Button],
+  templateUrl: './error-404.html',
+  styleUrl: './error-404.scss',
+})
+export class Error404 {}
